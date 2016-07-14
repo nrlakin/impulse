ee89c30f45341351d8ea7b0ac6961ec4dacd7f50
Neil Lakin <neil.lakin@gmail.com>
2016-07-14 13:14:25 -0400

    sqlalchemy working, building models. add study qr code.

--- aware_models.py
+++ aware_models.py
@@ -1,7 +1,8 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.hybrid import hybrid_property
-from sqlalchemy import Column, Integer, String, create_engine
-from sqlalchemy.orm import sessionmaker, synonym
+from sqlalchemy import Column, Integer, String, create_engine, ForeignKey
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.types import TypeDecorator
 from sqlalchemy.dialects.mysql import DOUBLE, TEXT
 from datetime import datetime
 import os
@@ -13,11 +14,23 @@ Session = sessionmaker(bind=engine)
 def get_session():
     return Session()
 
+class DoubleTimestamp(TypeDecorator):
+    impl = DOUBLE
+
+    def __init__(self):
+        TypeDecorator.__init__(self, asdecimal=False)
+
+    def process_bind_param(self, value, dialect):
+        return (value-datetime(1970, 1, 1)).total_seconds()*1000.
+
+    def process_result_value(self, value, dialect):
+        return datetime.utcfromtimestamp(float(value)/1000.)
+
 class Device(Base):
     __tablename__ = "aware_device"
 
     _id = Column(Integer, primary_key=True)
-    _timestamp = Column("timestamp", DOUBLE)
+    timestamp = Column(DoubleTimestamp)
     device_id = Column(String(150))
     board = Column(TEXT)
     brand = Column(TEXT)
@@ -32,21 +45,31 @@ class Device(Base):
     release_type = Column(TEXT)
     sdk = Column(Integer)
     label = Column(TEXT)
-
-    @hybrid_property
-    def timestamp(self):
-        return datetime.utcfromtimestamp(float(self._timestamp)/1000.)
-
-    @timestamp.setter
-    def timestamp(self, dt):
-        self._timestamp = DOUBLE(float(dt.strftime("%s"))*1000.)
+    # calls = relationship("Call", backref="device", lazy="dynamic")
 
     def __repr__(self):
-        return '<Device "%s">' % self.device_id
+        return '<Device "%d" Device "%s">' % (self._id, self.device_id)
 
-    # timestamp = synonym('_timestamp', descriptor=timestamp)
+class Call(Base):
+    __tablename__ = "calls"
+
+    _id = Column(Integer, primary_key=True)
+    timestamp = Column(DoubleTimestamp)
+    device_id = Column(String(150))
+    call_type = Column(Integer)
+    call_duration = Column(Integer)
+    trace = Column(TEXT)
+
+    def __repr__(self):
+        return '<Call "%s">' % self._id
 
 class Screen(Base):
     __tablename__ = "screen"
 
     _id = Column(Integer, primary_key=True)
+    timestamp = Column(DoubleTimestamp)
+    device_id = Column(String(150))
+    screen_status = Column(Integer)
+
+    def __repr__(self):
+        return '<Screen "%d" Device "%s">' % (self._id, self.device_id)
